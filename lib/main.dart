ac7ae94690f6302e818ee2069bd2bcb446d2f624
Marija Pavlichkovska <139142270+marijapavlichkovska@users.noreply.github.com>
2026-03-23 23:07:49 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 
 const String kBaseUrl = 'https://api.rawg.io/api';
-const String kRawgApiKey = 'xx';
+const String kRawgApiKey = 'xxx';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
